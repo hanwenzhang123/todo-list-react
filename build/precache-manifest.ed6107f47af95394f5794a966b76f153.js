4ceffa4b727df56664b22dbf8388012511dabb3f
Hanwen Zhang <hzhang8@bu.edu>
2021-07-21 22:17:20 -0400

    deployment

---
+++ build/precache-manifest.ed6107f47af95394f5794a966b76f153.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8351fa424bf6d3ca4ecc7dad40aa1569",
+    "url": "/hanwenzhang123/todo-list-react/index.html"
+  },
+  {
+    "revision": "e5f2aec77137107e26d1",
+    "url": "/hanwenzhang123/todo-list-react/static/js/2.c475bd67.chunk.js"
+  },
+  {
+    "revision": "0431cd929589d1405efa",
+    "url": "/hanwenzhang123/todo-list-react/static/js/main.5293aa17.chunk.js"
+  },
+  {
+    "revision": "86d7232d81b4b514d511",
+    "url": "/hanwenzhang123/todo-list-react/static/js/runtime-main.ebf01616.js"
+  }
+]);
